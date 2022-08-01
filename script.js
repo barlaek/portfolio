ea5c7f578eba02342f48ffda104a0fb80801e626
Liridona <91525128+barlaek@users.noreply.github.com>
2022-08-01 19:54:22 +0200

    js

---
+++ script.js
@@ -0,0 +1,13 @@
+function find_max(nums) {
+    let max_num = Number.NEGATIVE_INFINITY;
+    for (let num of nums) {
+        if (num > max_num) {
+            max_num = num;
+        }
+    }
+    return max_num;
+}
+
+find_max();
+
+document.getElementById
